a9fb1f2fbf494baf8703297901f33d7b2a5c676b
juma-moringa <allan.juma@student.moringaschool.com>
2021-05-14 16:28:57 +0300

    updated the component class

--- Quotes/src/app/quote.ts
+++ Quotes/src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+  showQuoteDetails: boolean;
+  constructor(
+    public id: number,
+    public datePosted: Date,
+    public userName: string,
+    public author: string,
+    public quote: string,
+    public upvote: number,
+    public downvote: number,
+    public mostPopular: boolean
+  ) {
+    this.showQuoteDetails = false;
+  }
 }
